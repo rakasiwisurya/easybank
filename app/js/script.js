b1e3c13148b54beb602af23b3547ce5317ff4236
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-02-14 10:28:43 +0700

    Animated Mobile Menu
    Set menu Home, About, Contact, Blog, and Careers and also set up  hamburger menu toggle to disable scrolling when hamburger menu toggle has clicked

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,26 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function(){
-    console.log('open hamburger');
+btnHamburger.addEventListener("click", function () {
+  console.log("open hamburger");
 
-    if (header.classList.contains('open')) { // Close Hamburger Menu
-        header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-    }
-    else { // Open Hamburger Menu
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-    }
-})
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
